a50a1317858cda6282fdc4fcb0e722ddd507016e
GustavoGil13 <gustavogil@ua.pt>
2020-11-10 13:00:40 +0000

    update

--- procstat.sh
+++ procstat.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 
-declare -a list_pid_info
-declare -a list_of_lists
+# declare -a list_pid_info
+# declare -a list_of_lists
+
+# mudar para a diretoria /proc
 cd /proc/
 
-function get_info {
-    
-    COMM=$(awk '/Name:/ {print $2}' status) # nome do pid
-    USER=$(whoami) # user name
-    PID=$(awk '/^Pid:/' status | tr -dc '0-9') # pid
-    MEM=$(awk '/VmSize:/' status | tr -dc '0-9') # quantidade de memoria total
-    RSS=$(awk '/VmRSS:/' status | tr -dc '0-9') # quantidade de memoria residente em memoria fisica
-    READB=$(sudo awk '/rchar:/' io | tr -dc '0-9') # numero total de bytes de Input
-    WRITEB=$(sudo awk '/wchar:/' io | tr -dc '0-9') # numero total de bytes de Ouput
-    # RATER ??
-    # RATEW ??
-    list_pid_info=($COMM $USER $PID $MEM $RSS $READB $WRITEB)
+# current_dir="$(pwd)/info.txt"
 
-    return 0
-}
-cd 1
-get_info
-echo ${list_pid_info[*]}
+# COMM=$(awk '/Name:/ {print $2}' status) # nome do pid
+#USER= ??# user name
+# PID=$(awk '/^Pid:/' status | tr -dc '0-9') # pid
+# MEM=$(awk '/VmSize:/' status | tr -dc '0-9') # quantidade de memoria total
+# RSS=$(awk '/VmRSS:/' status | tr -dc '0-9') # quantidade de memoria residente em memoria fisica
+# READB=$(awk '/rchar:/' io | tr -dc '0-9') # numero total de bytes de Input
+# WRITEB=$(awk '/wchar:/' io | tr -dc '0-9') # numero total de bytes de Ouput
+#RATER=??
+#RATEW=??
 
-# for pid in */; do
-#     echo $pid
-# done
+# string="$COMM\t$USER\t$PID\t$MEM\t$RSS\t$READB\t$WRITEB\t$RATERR\t$RATEW"
+
+# echo -e $string > $current_dir
+# cat $current_dir
+
+# começar por ver quais e que temos autorização
+
+for pid in */; do
+    echo $pid
+    COMM=$(awk '/Name:/ {print $2}' $pid/status) # nome do pid
+    echo $COMM
+done
