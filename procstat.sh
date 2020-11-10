93095b86579e2ba4ef654fad0d4d72a4e76c8524
GustavoGil13 <gustavogil@ua.pt>
2020-11-10 19:37:48 +0000

    tudo so falta argumentos

--- procstat.sh
+++ procstat.sh
@@ -1,39 +1,58 @@
 #!/bin/bash
 
-# declare -a list_pid_info
-# declare -a list_of_lists
+declare -a list_pid_info
+declare -a read_write
+declare -a pid_list
+
+LC_ALL=en_US.utf8
+
+# HEADER=("COMM" "USER" "PID" "MEM" "RSS" "READB" "WRITEB" "RATER" "RATEW" "DATE")
+current_dir="$(pwd)/info.txt"
 
 # mudar para a diretoria /proc
 cd /proc/
+segundos=2
 
-# current_dir="$(pwd)/info.txt"
-
-# COMM=$(awk '/Name:/ {print $2}' status) # nome do pid
-# USER= ?? ps -o uname= -p "${pid}"
-# PID=$(awk '/^Pid:/' status | tr -dc '0-9') # pid
-# MEM=$(awk '/VmSize:/' status | tr -dc '0-9') # quantidade de memoria total
-# RSS=$(awk '/VmRSS:/' status | tr -dc '0-9') # quantidade de memoria residente em memoria fisica
-# READB=$(awk '/rchar:/' io | tr -dc '0-9') # numero total de bytes de Input
-# WRITEB=$(awk '/wchar:/' io | tr -dc '0-9') # numero total de bytes de Ouput
-#RATER=??
-#RATEW=??
-
-# string="$COMM\t$USER\t$PID\t$MEM\t$RSS\t$READB\t$WRITEB\t$RATERR\t$RATEW"
-
-# echo -e $string > $current_dir
-# cat $current_dir
-
-# tenho de tirar / de pid
-
+i=0
+j=0
 for pid in */; do
-    if [[ -r "$pid/io" ]] && [[ -r "$pid/status" ]];then # checa se tanto io como status sao readable
-        echo $pid # pid
-        COMM=$(awk '/Name:/ {print $2}' $pid/status)
-        USER="$( ps -o uname= -p "${pid}" )"
+    if [[ -r "$pid/io" ]] && [[ -r "$pid/status" ]]; then
+        pid_list[$i]=$pid
         READB=$(awk '/rchar:/' $pid/io | tr -dc '0-9')
-        echo $COMM
-        echo $USER
-        echo $READB
-        echo -e "\n"
+        WRITEB=$(awk '/wchar:/' $pid/io | tr -dc '0-9')
+        read_write[$j]=$READB
+        read_write[$(( $j+1 ))]=$WRITEB
+        i=$(( $i+1 ))
+        j=$(( $j+2 ))
     fi
 done
+
+sleep $segundos
+# echo ${pid_list[*]}
+# echo ${read_write[*]}
+
+i=0
+for pid in "${pid_list[@]}"; do
+
+    PID=$(perl -pe 's/\///g' <<< "$pid") 
+    COMM=$(awk '/Name:/ {print $2}' $pid/status)
+    USER="$( ps -o uname= -p "${PID}" )"
+    MEM=$(awk '/VmSize:/' $pid/status | tr -dc '0-9') # quantidade de memoria total
+    RSS=$(awk '/VmRSS:/' $pid/status | tr -dc '0-9') # quantidade de memoria residente em memoria fisica
+    # DATE=??
+    
+    READB1=${read_write[$i]}
+    WRITEB1=${read_write[$(( $i+1 ))]}
+    READB2=$(awk '/rchar:/' $pid/io | tr -dc '0-9') # numero total de bytes de Input
+    WRITEB2=$(awk '/wchar:/' $pid/io | tr -dc '0-9') # numero total de bytes de Ouput
+
+    RATER=$( bc -l <<< $(( READB2 - READB1 ))/$segundos )
+    RATEW=$( bc -l <<< $(( WRITEB2 - WRITEB1 ))/$segundos )
+
+    printf "%-20s %-10s %10s %10s %10s %15s %15s %15.2f %15.2f\n" $COMM $USER $PID $MEM $RSS $READB2 $WRITEB2 $RATER $RATEW >> $current_dir # $DATE
+    i=$(( $i+2 ))
+done
+
+printf "%-20s %-10s %10s %10s %10s %15s %15s %15s %15s %15s\n" COMM USER PID MEM RSS READB WRITEB RATER RATEW DATE
+cat $current_dir
+rm $current_dir
